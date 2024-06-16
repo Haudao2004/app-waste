b4ea7b3e1090cb15649fb28f8cfd3b2b628f3c0a
Haudao2004 <daohau300404@gmail.com>
2024-06-16 11:05:16 +0700

    test

--- waste.py
+++ waste.py
@@ -6,14 +6,18 @@ from dotenv import load_dotenv
 import os
 import openai
 import json
-# from config import OPENAI_API_KEY
-# import openai
-# from flask import Flask, render_template
+from flask_ckeditor import CKEditor
+from flask import Flask, render_template
+
 
 # -*- coding: utf-8 -*-
 
+
+
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
+app=Flask(__name__)
+ckeditor=CKEditor(app)
 
 
 # def load_api_key(secrets_file="secrets.json"):
